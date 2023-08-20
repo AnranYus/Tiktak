fb8daeb74fecbb486e6f2a480fd1f656da68da29
Anran <anranyus@hotmail.com>
2023-08-20 20:31:33 +0800

    chore: 输出请求日志

---
+++ Gateway/src/main/java/com/anyus/gateway/filter/MyGlobalFilter.java
@@ -0,0 +1,37 @@
+package com.anyus.gateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.util.Objects;
+
+@Component
+
+public class MyGlobalFilter implements GlobalFilter {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        URI origin = request.getURI();
+        HttpMethod method = request.getMethod();
+        MultiValueMap<String, String> queryParams = request.getQueryParams();
+        logger.info(method+":"+origin+"/"+queryParams);
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            ServerHttpResponse response = exchange.getResponse();
+            // 打印响应信息
+            logger.info("Response:"+Objects.requireNonNull(response.getStatusCode()).value());
+        }));
+    }
+}
