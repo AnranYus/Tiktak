4019bec7bcf94fc8831127a526bff3b4386d413a
Anran <anranyus@hotmail.com>
2023-08-20 20:24:36 +0800

    fix(login): uid为0时直接使用token获取uid

--- User/src/main/java/com/anryus/userservice/UserServiceApplication.java
+++ User/src/main/java/com/anryus/userservice/UserServiceApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @MapperScan("com.anryus.userservice.mapper")
+@ComponentScan({"com.anryus.common","com.anryus.userservice"})
 public class UserServiceApplication {
 
     public static void main(String[] args) {
--- User/src/main/java/com/anryus/userservice/controller/UserController.java
+++ User/src/main/java/com/anryus/userservice/controller/UserController.java
@@ -2,15 +2,14 @@ package com.anryus.userservice.controller;
 
 import com.anryus.common.entity.Rest;
 import com.anryus.common.entity.User;
+import com.anryus.common.utils.JwtUtils;
 import com.anryus.userservice.entity.UserRest;
 import com.anryus.userservice.service.UserService;
 import com.anryus.userservice.utils.Jwt;
 import org.springframework.lang.Nullable;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@Controller
 @ResponseBody
 public class UserController {
 
@@ -19,20 +18,31 @@ public class UserController {
     final
     Jwt jwt;
 
-    public UserController(UserService userService, Jwt jwt) {
+    final JwtUtils jwtUtils;
+
+    public UserController(UserService userService, Jwt jwt,JwtUtils jwtUtils) {
         this.userService = userService;
         this.jwt = jwt;
+        this.jwtUtils = jwtUtils;
     }
 
 
     @GetMapping("/douyin/user/")
     public Rest<User> getUserInfo(@RequestParam("user_id") long uid, @RequestParam("token")@Nullable String token){
+        if (uid == 0){
+            String uidStr = jwtUtils.verify(token).get("uid");
+            if (uidStr!=null){
+                uid = Long.parseLong(uidStr);
+            }else {
+                return Rest.fail("不存在该用户");
+            }
+        }
 
         User userByUid = userService.getUserByUid(uid);
         if (userByUid != null){
-            return Rest.success("成功",userByUid);
+            return Rest.success("成功","user" ,userByUid);
         }else {
-            return Rest.fail("获取用户信息失败",null);
+            return Rest.fail("获取用户信息失败");
         }
     }
 
@@ -53,13 +63,8 @@ public class UserController {
         User user = userService.login(username, password);
 
         if (user!= null){
-            String token = jwt.getToken(user.getUid());
-            if (token != null){
-                return UserRest.success("", user.getUid(), token);
-            }else {
-                String token1 = jwt.createToken(user.getUid(), "user");
-                return UserRest.success("",user.getUid(),token1);
-            }
+            String token = jwt.createToken(user.getUid(), "user");
+            return UserRest.success("",user.getUid(),token);
         }
 
         return UserRest.fail("账户或密码错误");
