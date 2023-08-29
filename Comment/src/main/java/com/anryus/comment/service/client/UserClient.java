485c5c789ced4b6a79514f1b58b14c93dfc0d342
Anran <anranyus@hotmail.com>
2023-08-30 02:19:12 +0800

    refactor: 重构模块依赖和接口

--- Comment/src/main/java/com/anryus/comment/service/client/UserClient.java
+++ Comment/src/main/java/com/anryus/comment/service/client/UserClient.java
@@ -6,11 +6,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Component
 @FeignClient("service-user")
 public interface UserClient {
     @GetMapping("/douyin/user/")
-    Rest<User> getUserInfo(@RequestParam("user_id") long uid, @RequestParam("token")@Nullable String token);
+    Rest<User> getUserInfo(@RequestParam("user_id") long uid, @RequestHeader("user-id")@Nullable Long requestUid);
 }
--- Feed/src/main/java/com/anryus/feed/service/client/FavoriteClient.java
+++ Feed/src/main/java/com/anryus/feed/service/client/FavoriteClient.java
@@ -1,9 +1,9 @@
 package com.anryus.feed.service.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient( value ="service-favorite")
@@ -11,5 +11,5 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface FavoriteClient {
 
     @GetMapping("/douyin/favorite/is")
-    boolean isFavorite(@RequestParam("user_id")Long userId,@RequestParam("video_id") Long videoId,@RequestParam("token")@Nullable String token);
+    boolean isFavorite(@RequestParam("user_id")Long userId,@RequestParam("video_id") Long videoId,@RequestHeader("user-id")long requestUid);
 }
