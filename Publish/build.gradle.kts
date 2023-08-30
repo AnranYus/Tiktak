c299e6c1f612b9c591719605d4c7a2d92757b0b6
Anran <anranyus@hotmail.com>
2023-08-30 13:42:33 +0800

    fix(publish): 补全缺失依赖

--- Publish/build.gradle.kts
+++ Publish/build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-amqp:3.1.3")
+    implementation("software.amazon.awssdk:s3:2.20.131")
     implementation("com.alibaba.cloud:spring-cloud-starter-alibaba-nacos-config:2022.0.0.0")
     implementation("org.springframework.cloud:spring-cloud-starter-bootstrap:4.0.4")
     implementation(project(mapOf("path" to ":Common")))
