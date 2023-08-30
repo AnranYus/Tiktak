d077c12074a5fa22e34ae6a6d294fe69f48a8567
Anran <anranyus@hotmail.com>
2023-08-30 13:50:47 +0800

    fix(relation): 去除多余依赖

--- Relation/src/main/java/com/anryus/relation/RelationApplication.java
+++ Relation/src/main/java/com/anryus/relation/RelationApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 @MapperScan("com.anryus.relation.mapper")
 @EnableDiscoveryClient
 @EnableFeignClients
-@ComponentScan({"com.anryus.common","com.anryus.relation"})
 @SpringBootApplication
 public class RelationApplication {
     public static void main(String[] args) {
